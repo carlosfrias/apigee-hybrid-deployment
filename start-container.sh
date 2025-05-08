8e10c0b2c62cff8f21595f31210857684478a5a7
Carlos Frias <friasc@google.com>
2025-05-08 10:09:00 -0400

    updated config

--- start-container.sh
+++
@@ -1,8 +0,0 @@
-#bash
-docker run -v $HOME/.apigee-secure:/root/.apigee-secure:ro \
-   -v $(pwd)/resources:/apigee-workspace/apigee-helm/resources:ro \
-   -v $(pwd)/molecule:/apigee-workspace/apigee-helm/molecule:rw \
-   -v $(pwd)/work_dir:/apigee-workspace/apigee-helm/work_dir:rw \
-   -v $HOME/.config:/root/.config/gcloud:rw \
-   -ti \
-   us-east1-docker.pkg.dev/friasc-apigee-lab/apigee-workspace/apigee-workspace:v1.0 bash
